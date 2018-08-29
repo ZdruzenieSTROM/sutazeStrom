1b38c0d54779627f6030cf9f9de95af0fa0730ba
MartinStevko <mstevko10@gmail.com>
2018-08-29 23:08:56 +0200

    Initial configuration

--- odc_server/settings.py
+++ odc_server/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.contrib import admin
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,6 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+APPEND_SLASH = True
+
 
 # Application definition
 
@@ -30,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'competition.apps.CompetitionConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,5 +110,18 @@ from odc_server.local_settings import *
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+### Deployment ### + set DEBUG = False
+'''
+STATIC_ROOT = '/var/www/repreU17.pythonanywhere.com/static/'
+# for collectstatic command - '/home/repreU17/U17-diary/static/'
+'''
+##################
+
+### Production ###
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+##################
+
+admin.site.site_url = ''
 
 STATIC_URL = '/static/'
--- odc_server/urls.py
+++ odc_server/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('competition.urls', namespace='competition')),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
