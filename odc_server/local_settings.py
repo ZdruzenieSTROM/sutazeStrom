2717a7ad5ec1a5cd8f0ef257496c11be0dbe4209
Martin Števko <mstevko10@gmail.com>
2018-08-29 22:15:44 +0200

    Init

---
+++ odc_server/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'make-your-own-secret-key' # 50 characters
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Bratislava'
