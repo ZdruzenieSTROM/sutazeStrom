c868ace38cc0af76b2f4ce5fc0eafe5d50030f0a
MartinStevko <mstevko10@gmail.com>
2018-08-29 23:21:26 +0200

    ...

--- odc_server/local_settings.py
+++ odc_server/local_settings.py
@@ -1,6 +1,6 @@
 ### Local project settings ###
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'make-your-own-secret-key' # 50 characters
+SECRET_KEY = '' # 50 characters
 
 DEBUG = True
 
