f3e33661d77eb39de2b09bc53b672715a0040345
MartinStevko <mstevko10@gmail.com>
2018-09-10 00:01:58 +0200

    Control sum

--- competition/views.py
+++ competition/views.py
@@ -20,37 +20,51 @@ class EventDetailView(DetailView):
 
 def submit(request, pk):
     event = Event.objects.get(pk=pk)
+    template = 'competition/submit.html'
 
     if request.method == 'POST':
         form = SubmitForm(request.POST)
 
         if form.is_valid():
             try:
-                barcode = form.cleaned_data['code'][:5]
+                barcode = form.cleaned_data['code']
             except(IndexError):
                 form = SubmitForm()
-                return render(request, 'competition/submit.html', {'error':True, 'form': form, 'event': event})
+                return render(request, template, {'error':True, 'form': form, 'event': event})
 
             if len(barcode) == 6:
-                control_digit = int(form.cleaned_data['code'][-1])
-                control_sum = int(barcode[0])*control[0] + int(barcode[1])*control[1] + int(barcode[2])*control[2] + int(barcode[3])*control[3] + int(barcode[4])*control[4]
+                barcode = barcode[:5]
+                try:
+                    control_digit = int(form.cleaned_data['code'][-1])
+                    control_sum = int(barcode[0])*control[0] + int(barcode[1])*control[1] + int(barcode[2])*control[2] + int(barcode[3])*control[3] + int(barcode[4])*control[4]
+                except(ValueError):
+                    form = SubmitForm()
+                    return render(request, template, {'error':True, 'form': form, 'event': event})
 
                 if control_digit == (control_sum % 10):
-                    team = Team.objects.get(number=int(barcode[:3]))
-                    problem = Problem.objects.get(event=event, position=int(barcode[3:5]))
+                    #team = Team.objects.get(number=int(barcode[:3]))
+                    #problem = Problem.objects.get(event=event, position=int(barcode[3:5]))
 
-                    Solution.objects.create(event=event, problem=problem, team=team)
+                    #Solution.objects.create(event=event, problem=problem, team=team)
 
+                    request.session['success'] = True
                     return redirect('competition:submit', pk=pk)
                 else:
                     form = SubmitForm()
-                    return render(request, 'competition/submit.html', {'error':True, 'form': form, 'event': event})
+                    return render(request, template, {'error':True, 'form': form, 'event': event})
 
             else:
                 form = SubmitForm()
-                return render(request, 'competition/submit.html', {'error':True, 'form': form, 'event': event})
+                return render(request, template, {'error':True, 'form': form, 'event': event})
+        else:
+            return render(request, template, {'error':True, 'form': form, 'event': event})
 
     else:
         form = SubmitForm()
-
-        return render(request, 'competition/submit.html', {'form': form, 'event': event})
+        try:
+            success = request.session['success']
+            del request.session['success']
+        except:
+            return render(request, template, {'form': form, 'event': event})
+        else:
+            return render(request, template, {'success':success, 'form': form, 'event': event})
