466de0ecbcb4b620a9def51dbaf84e8538eb5a9f
MartinStevko <mstevko10@gmail.com>
2018-09-10 00:09:38 +0200

    Cursor focusing into form input

--- competition/forms.py
+++ competition/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class SubmitForm(forms.Form):
     code = forms.CharField(max_length=6, label='')
+
+    def __init__(self, *args, **kwargs):
+        super(SubmitForm, self).__init__(*args, **kwargs)
+        self.fields['code'].widget.attrs.update({'id': 'barcode_input'})
