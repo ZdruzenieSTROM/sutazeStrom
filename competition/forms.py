aeb293b7f0b88aafd8ca43bd75ff7dd7095befb1
MartinStevko <mstevko10@gmail.com>
2018-09-10 20:11:32 +0200

    Styling details

--- competition/forms.py
+++ competition/forms.py
@@ -17,14 +17,14 @@ class SubmitForm(forms.Form):
             code = [int(c) for c in self.cleaned_data['code']]
 
         except ValueError:
-            raise ValidationError('Nesprávny formát', code='invalid_format')
+            raise ValidationError('Nesprávny formát!', code='invalid_format')
 
         if len(code) != 6:
-            raise ValidationError('Nesprávna dĺžka kódu', code='invalid_length')
+            raise ValidationError('Nesprávna dĺžka kódu!', code='invalid_length')
 
         control_digit, code = code[-1], code[:-1]
 
         if control_digit != reduce(lambda p, n: p + n[0]*n[1], zip(code, CONTROL), 0) % 10:
-            raise ValidationError('Neplatný kontrolný súčet', code='invalid_control_sum')
+            raise ValidationError('Neplatný kontrolný súčet!', code='invalid_control_sum')
 
         return code
