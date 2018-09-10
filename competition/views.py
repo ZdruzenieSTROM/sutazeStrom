793e7270a5b91fa3a4c0a658b7a73222890bcd63
MartinStevko <mstevko10@gmail.com>
2018-09-10 20:12:56 +0200

    Error messages

--- competition/views.py
+++ competition/views.py
@@ -29,6 +29,9 @@ def submit(request, pk):
 
         if form.is_valid():
             code = form.cleaned_data['code']
+            code_string = ''
+            for digit in code:
+                code_string += str(digit)
 
             convert = lambda l: reduce(lambda p, n: p*10 + n, l)
             number, position = convert(code[:3]), convert(code[3:])
@@ -38,22 +41,24 @@ def submit(request, pk):
                 problem = Problem.objects.get( event=event, position=position)
 
             except Team.DoesNotExist:
-                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platný tím')
+                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platný tím! Prečítané číslo tímu je {} a tento tím nie je do súťaže registrovaný. #{}'.format(number, code_string))
 
             except Problem.DoesNotExist:
-                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platnú úlohu')
+                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platnú úlohu! Prečítané číslo úlohy je {} a táto úloha v súťaži nie je evidovaná. #{}'.format(position, code_string))
 
             else:
                 try:
-                    Solution.objects.get(event=event, team=team, problem=problem)
+                    solution = Solution.objects.get(event=event, team=team, problem=problem)
 
                 except Solution.DoesNotExist:
-                    Solution.objects.create(event=event, team=team, problem=problem)
-                    messages.add_message(request, messages.SUCCESS, 'Úloha bola úspešne odovzdaná')
+                    solution = Solution.objects.create(event=event, team=team, problem=problem)
+                    messages.add_message(request, messages.SUCCESS, 'OK! Úloha {} bola úspešne odovzdaná tímom zo školy {}.'.format(solution.problem.position, solution.team.school.name))
                     form = SubmitForm()
 
+                    return redirect('competition:submit')
+
                 else:
-                    messages.add_message(request, messages.ERROR, 'Táto úloha už bola odovzdaná')
+                    messages.add_message(request, messages.ERROR, 'Táto úloha už bola odovzdaná! Stalo sa tak v čase {}. #{}'.format(solution.time, code_string))
 
     else:
         form = SubmitForm()
