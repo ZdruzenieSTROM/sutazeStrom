fcc9430c2e734275413933e1988400d6f801647b
Martin Mihálik <9matko.mihalik9@gmail.com>
2018-09-11 20:22:49 +0200

    Results view implementation

--- competition/urls.py
+++ competition/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import EventListView, EventDetailView, submit
+from .views import EventListView, EventDetailView, submit, results
 
 app_name = 'competition'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', EventListView.as_view(), name='index'),
     path('<int:pk>/', EventDetailView.as_view(), name='event'),
     path('<int:pk>/submit', submit, name='submit'),
+    path('<int:pk>/results', results, name='results'),
 ]
--- competition/views.py
+++ competition/views.py
@@ -61,3 +61,62 @@ def submit(request, pk):
         form = SubmitForm()
 
     return render(request, template_name, {'form': form, 'event': event})
+
+def results(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    template_name = 'competition/results.html'
+
+    results = Solution.objects.raw('''
+    SELECT
+        participant_team.id AS id,
+        participant_team.name AS team_name,
+        participant_school.name AS school_name,
+        participant_school.address AS school_address,
+        participant_school.city AS school_city,
+        members.members AS team_members,
+        SUM(competition_problem.points) AS points,
+        hardest_position.position AS hardest_position,
+        hardest_time.time AS hardest_time
+    FROM competition_solution
+    JOIN participant_team ON competition_solution.team_id = participant_team.id
+    JOIN competition_problem ON competition_solution.problem_id = competition_problem.id
+    JOIN participant_school ON participant_team.school_id = participant_school.id
+    LEFT OUTER JOIN (
+        SELECT
+            GROUP_CONCAT(participants.full_name, ", ") AS members,
+            participants.team_id
+        FROM (
+            SELECT
+                participant_participant.first_name || " " || participant_participant.last_name AS full_name,
+                participant_participant.team_id
+            FROM
+                participant_participant
+        ) AS participants
+        GROUP BY participants.team_id
+    ) AS members ON competition_solution.team_id = members.team_id
+    JOIN (
+        SELECT
+            MAX(competition_problem.position) AS position,
+            competition_solution.team_id
+        FROM competition_solution
+        JOIN competition_problem ON competition_solution.problem_id = competition_problem.id
+        GROUP BY competition_solution.team_id
+    ) AS hardest_position ON competition_solution.team_id = hardest_position.team_id
+    JOIN  (
+        SELECT
+            competition_solution.time AS time,
+            competition_solution.team_id
+        FROM competition_solution
+        JOIN competition_problem ON competition_solution.problem_id = competition_problem.id
+        GROUP BY competition_solution.team_id
+        HAVING competition_problem.position = MAX(competition_problem.position)
+    ) AS hardest_time ON competition_solution.team_id = hardest_time.team_id
+    WHERE competition_solution.event_id = %s
+    GROUP BY competition_solution.team_id
+    ORDER BY
+        points DESC,
+        hardest_position DESC,
+        hardest_time ASC
+    ''', [pk])
+
+    return render(request, template_name, {'event': event, 'results': results})
