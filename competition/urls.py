036d9185af54361c781d464feeca7756e1e64715
Martin Mihálik <9matko.mihalik9@gmail.com>
2018-09-14 16:33:52 +0200

    Class-based views

--- competition/urls.py
+++ competition/urls.py
@@ -1,12 +1,13 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
-from .views import EventDetailView, EventListView, results, submit
+from .views import EventDetailView, EventListView, ResultsView, SubmitFormView
 
 app_name = 'competition'
 
 urlpatterns = [
-    path('', EventListView.as_view(), name='index'),
-    path('<int:pk>/', EventDetailView.as_view(), name='event'),
-    path('<int:pk>/submit', submit, name='submit'),
-    path('<int:pk>/results', results, name='results'),
+    path('', staff_member_required(EventListView.as_view()), name='index'),
+    path('<int:pk>/', staff_member_required(EventDetailView.as_view()), name='event'),
+    path('<int:pk>/submit', staff_member_required(SubmitFormView.as_view()), name='submit'),
+    path('<int:pk>/results', staff_member_required(ResultsView.as_view()), name='results'),
 ]
--- competition/views.py
+++ competition/views.py
@@ -1,9 +1,10 @@
 from functools import reduce
 
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import DetailView, ListView
 from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.generic import DetailView, FormView, ListView, TemplateView
+from django.views.generic.detail import SingleObjectMixin
 
 from participant.models import Team
 
@@ -13,62 +14,65 @@ from .queries import results_query
 
 class EventListView(ListView):
     model = Event
-    template_name = 'competition/index.html'
     context_object_name = 'events'
+
+    template_name = 'competition/index.html'
 
 class EventDetailView(DetailView):
     model = Event
-    template_name = 'competition/event.html'
     context_object_name = 'event'
 
-@staff_member_required
-def submit(request, pk):
-    event = get_object_or_404(Event, pk=pk)
+    template_name = 'competition/event.html'
+
+class SubmitFormView(FormView, DetailView):
+    model = Event
+    context_object_name = 'event'
+
     template_name = 'competition/submit.html'
 
-    if request.method == 'POST':
-        form = SubmitForm(request.POST)
+    form_class = SubmitForm
 
-        if form.is_valid():
-            code = form.cleaned_data['code']
+    def get_success_url(self):
+        return reverse('competition:submit', kwargs={'pk': self.kwargs['pk']})
 
-            convert = lambda l: reduce(lambda p, n: p*10 + n, l)
-            number, position, code_string = convert(code[:3]), convert(code[3:]), str(convert(code))
+    def form_valid(self, form):
+        event = get_object_or_404(Event, pk=int(self.kwargs['pk']))
+        code = form.cleaned_data['code']
 
+        convert = lambda l: reduce(lambda p, n: p*10 + n, l)
+        number, position, code_string = convert(code[:3]), convert(code[3:]), str(convert(code))
+
+        try:
+            team = Team.objects.get(number=number)
+            problem = Problem.objects.get(event=event, position=position)
+
+        except Team.DoesNotExist:
+            messages.add_message(self.request, messages.ERROR, 'Kód neobsahuje platný tím! Prečítané číslo tímu je {} a tento tím nie je do súťaže registrovaný. #{}'.format(number, code_string))
+
+        except Problem.DoesNotExist:
+            messages.add_message(self.request, messages.ERROR, 'Kód neobsahuje platnú úlohu! Prečítané číslo úlohy je {} a táto úloha v súťaži nie je evidovaná. #{}'.format(position, code_string))
+
+        else:
             try:
-                team = Team.objects.get(number=number)
-                problem = Problem.objects.get(event=event, position=position)
+                solution = Solution.objects.get(event=event, team=team, problem=problem)
 
-            except Team.DoesNotExist:
-                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platný tím! Prečítané číslo tímu je {} a tento tím nie je do súťaže registrovaný. #{}'.format(number, code_string))
-
-            except Problem.DoesNotExist:
-                messages.add_message(request, messages.ERROR, 'Kód neobsahuje platnú úlohu! Prečítané číslo úlohy je {} a táto úloha v súťaži nie je evidovaná. #{}'.format(position, code_string))
+            except Solution.DoesNotExist:
+                solution = Solution.objects.create(event=event, team=team, problem=problem)
+                messages.add_message(self.request, messages.SUCCESS, 'OK! Úloha {} bola úspešne odovzdaná tímom zo školy {}.'.format(solution.problem.position, solution.team.school.name))
 
             else:
-                try:
-                    solution = Solution.objects.get(event=event, team=team, problem=problem)
+                messages.add_message(self.request, messages.ERROR, 'Táto úloha už bola odovzdaná! Stalo sa tak v čase {}. #{}'.format(solution.time, code_string))
 
-                except Solution.DoesNotExist:
-                    solution = Solution.objects.create(event=event, team=team, problem=problem)
-                    messages.add_message(request, messages.SUCCESS, 'OK! Úloha {} bola úspešne odovzdaná tímom zo školy {}.'.format(solution.problem.position, solution.team.school.name))
-                    form = SubmitForm()
+        return super().form_valid(form)
 
-                    return redirect('competition:submit')
+class ResultsView(DetailView):
+    model = Event
+    context_object_name = 'event'
 
-                else:
-                    messages.add_message(request, messages.ERROR, 'Táto úloha už bola odovzdaná! Stalo sa tak v čase {}. #{}'.format(solution.time, code_string))
-
-    else:
-        form = SubmitForm()
-
-    return render(request, template_name, {'form': form, 'event': event})
-
-@staff_member_required
-def results(request, pk):
-    event = get_object_or_404(Event, pk=pk)
     template_name = 'competition/results.html'
 
-    results = Solution.objects.raw(results_query, [pk])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['results'] = Solution.objects.raw(results_query, [self.kwargs['pk']])
 
-    return render(request, template_name, {'event': event, 'results': results})
+        return context
