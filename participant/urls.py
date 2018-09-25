0072e079407d6f560db344c09c6433c982fb7ab3
Martin Mihálik <34107535+mmihalik@users.noreply.github.com>
2018-09-25 18:42:21 +0200

    Removed deprecated css classes

--- participant/urls.py
+++ participant/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
 from django.contrib.auth.decorators import login_required
+from django.urls import path
 
-from .views import ImportData, ExportData
+from .views import ExportData, ImportData
 
 app_name = 'participant'
 
